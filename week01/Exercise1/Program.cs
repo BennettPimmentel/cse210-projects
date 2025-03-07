db5b77449e927f12df37b1a86affee7f4991c697
BennettPimmentel <bennettpimentel007@gmail.com>
2025-03-07 18:02:05 -0500

    added

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main()
     {
-        // Solicitar el primer nombre al usuario
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        // Solicitar el apellido al usuario
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        // Mostrar el mensaje en el formato requerido
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
