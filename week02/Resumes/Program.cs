bc6feeec808ce7644b5a0a19bc8e3308e989d516
BennettPimmentel <bennettpimentel007@gmail.com>
2025-03-12 23:21:41 -0500

    added

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2015;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Logistic";
+        job2._company = "Google";
+        job2._startYear = 2010;
+        job2._endYear = 2015;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
