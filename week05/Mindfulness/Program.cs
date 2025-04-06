ca41dd5c0e1df6e71569cae7b19a99c5587ac8b8
BennettPimmentel <bennettpimentel007@gmail.com>
2025-04-06 02:28:16 -0400

    added

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,243 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+
+abstract class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(string name, string description)
+    {
+        _name = name;
+        _description = description;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name}!");
+        Console.WriteLine(_description);
+        Console.Write("Enter the duration in seconds: ");
+        _duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("\nWell done!");
+        ShowSpinner(2);
+        Console.WriteLine($"You have completed the {_name} for {_duration} seconds.");
+        ShowSpinner(3);
+    }
+
+    protected void ShowSpinner(int seconds)
+    {
+        string[] spinner = { "|", "/", "-", "\\" };
+        DateTime end = DateTime.Now.AddSeconds(seconds);
+        int i = 0;
+        while (DateTime.Now < end)
+        {
+            Console.Write(spinner[i % spinner.Length]);
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            i++;
+        }
+    }
+
+    protected void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+
+    public abstract void PerformActivity();
+}
+
+class BreathingActivity : Activity
+{
+    public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.") { }
+
+    public override void PerformActivity()
+    {
+        DisplayStartingMessage();
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine("\nBreathe in...");
+            ShowCountdown(4);
+            Console.WriteLine("Breathe out...");
+            ShowCountdown(6);
+        }
+
+        DisplayEndingMessage();
+    }
+}
+
+class ReflectionActivity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
+
+    private List<string> _questions = new List<string>
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"
+    };
+
+    public ReflectionActivity() : base("Reflection Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience.") { }
+
+    public override void PerformActivity()
+    {
+        DisplayStartingMessage();
+        Random rand = new Random();
+        Console.WriteLine("\n" + _prompts[rand.Next(_prompts.Count)]);
+        ShowSpinner(3);
+
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine(_questions[rand.Next(_questions.Count)]);
+            ShowSpinner(4);
+        }
+
+        DisplayEndingMessage();
+    }
+}
+
+class ListingActivity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    public ListingActivity() : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.") { }
+
+    public override void PerformActivity()
+    {
+        DisplayStartingMessage();
+        Random rand = new Random();
+        Console.WriteLine("\n" + _prompts[rand.Next(_prompts.Count)]);
+        Console.WriteLine("Start listing items when prompted...");
+        ShowCountdown(5);
+
+        int count = 0;
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            Console.ReadLine();
+            count++;
+        }
+
+        Console.WriteLine($"You listed {count} items!");
+        DisplayEndingMessage();
+    }
+}
+
+class AffirmationActivity : Activity
+{
+    private List<string> _affirmations = new List<string>
+    {
+        "You are capable of great things.",
+        "You are worthy of love and respect.",
+        "You have the power to create change.",
+        "Your presence matters.",
+        "You grow stronger every day.",
+        "You can handle whatever comes your way.",
+        "You are doing your best, and that is enough.",
+        "You are enough just as you are."
+    };
+
+    public AffirmationActivity() : base("Affirmation Activity", "This activity will help you focus on positive affirmations to boost your self-worth and confidence.") { }
+
+    public override void PerformActivity()
+    {
+        DisplayStartingMessage();
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        int index = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine("\n" + _affirmations[index]);
+            index = (index + 1) % _affirmations.Count;
+            ShowSpinner(3);
+        }
+
+        DisplayEndingMessage();
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        BreathingActivity breathing = new BreathingActivity();
+        ReflectionActivity reflection = new ReflectionActivity();
+        ListingActivity listing = new ListingActivity();
+        AffirmationActivity affirmation = new AffirmationActivity();
+
+        bool running = true;
+        while (running)
+        {
+            Console.Clear();
+            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Start Affirmation Activity");
+            Console.WriteLine("5. Quit");
+            Console.Write("Select an option: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    breathing.PerformActivity();
+                    break;
+                case "2":
+                    reflection.PerformActivity();
+                    break;
+                case "3":
+                    listing.PerformActivity();
+                    break;
+                case "4":
+                    affirmation.PerformActivity();
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
+
+        Console.WriteLine("Thanks for using the Mindfulness Program. Goodbye!");
     }
 }
