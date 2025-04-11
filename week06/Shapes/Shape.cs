73e7dfe87487766b8edda83bb4d0ff2ce17ea6b4
BennettPimmentel <bennettpimentel007@gmail.com>
2025-04-11 15:29:41 -0400

    added

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,18 @@
+using System;
+
+public abstract class Shape
+{
+    public string Color { get; set; }
+
+    public Shape(string color)
+    {
+        Color = color;
+    }
+
+    public abstract double GetArea();
+
+    public string GetColor()
+    {
+        return Color;
+    }
+}
